55369cad5f23841442e1c7ecb04da08a9520a268
SLOWIDEV <sakus1f@gmail.com>
2024-08-04 21:20:56 +0300

    add save file

--- src/main/java/com/saikos/texteditor/Editor.java
+++ src/main/java/com/saikos/texteditor/Editor.java
@@ -1,16 +1,17 @@
 package com.saikos.texteditor;
 
 import javafx.application.Application;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.*;
 
 public class Editor extends Application {
     public static Pane root;
@@ -21,23 +22,54 @@ public class Editor extends Application {
     public void start(Stage stage) throws Exception {
         anchorPane = new AnchorPane();
         Menu fileMenu = new Menu("File");
+        MenuItem saveFileItem = new MenuItem("Save file");
+        MenuItem openFileItem = new MenuItem("Open file");
+        fileMenu.getItems().addAll(saveFileItem, openFileItem);
+
         Menu editMenu = new Menu("Edit");
+        MenuItem setFontItem = new MenuItem("Set Font");
+        editMenu.getItems().addAll(setFontItem);
 
         MenuBar menuBar = new MenuBar();
         menuBar.getMenus().addAll(fileMenu, editMenu);
+
+        saveFileItem.setOnAction(actionEvent -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Save file");
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Editor", "*.txt"));
+            fileChooser.getInitialDirectory();
+            File file = fileChooser.showSaveDialog(stage);
+            File filePath = new File(file.getAbsolutePath());
+
+
+            try {
+                file.createNewFile();
+                file.mkdirs();
+
+                FileWriter fileWriter = new FileWriter(file);
+                fileWriter.write(textArea.getText());
+                fileWriter.close();
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+
+
 
 
 
         textArea = new TextArea();
         root = new Pane();
         Scene scene = new Scene(root, 800, 600);
-
         textArea.setPrefSize(800, 600);
         textArea.setLayoutY(27);
-
         root.getChildren().add(textArea);
         anchorPane.getChildren().add(menuBar);
         root.getChildren().add(anchorPane);
+
 
         stage.setTitle("Text Editor");
         stage.setScene(scene);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.saikos.texteditor {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.saikos.texteditor to javafx.fxml;
